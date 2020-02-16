990f557427798d79afa3e574735a32bc69fc0199
Adam Ryvola <adam.ryvola@adane.cz>
2020-02-16 14:33:25 +0100

    Merge branch 'master' of /Users/adamryvola/Develop/csas/abeceda-penez-admin with conflicts.

--- src/initApolloClient.js
+++ src/initApolloClient.js
@@ -5,7 +5,7 @@ import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
 import { persistCache } from 'apollo-cache-persist';
 
-const ENDPOINT = 'abeceda.adane.cz';
+const ENDPOINT = 'localhost:4000';
 
 export default async() => {
     /** INIT HTTPS & WS CONNECTION TO GRAPH-QL SERVER **/
@@ -23,12 +23,12 @@ export default async() => {
 
     // init HTTP connection
     // const httpLink = new HttpLink({ uri: `https://${ENDPOINT}` });
-    const httpLink = createUploadLink({ uri: `https://${ENDPOINT}` });
+    const httpLink = createUploadLink({ uri: `http://${ENDPOINT}` });
     const httpLinkAuth = middlewareLink.concat(httpLink);
 
     // init WebSocket connection
     const wsLink = new WebSocketLink({
-        uri: `wss://${ENDPOINT}`,
+        uri: `ws://${ENDPOINT}`,
         options: {
             reconnect: true,
             connectionParams: {
